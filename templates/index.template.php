3fa6674e35f4011144fc680922a058405aac1b77
RaulMOAB <raulmontoroabaijon@gmail.com>
2022-10-26 18:04:47 +0200

    Navbar added to template

--- templates/index.template.php
+++ templates/index.template.php
@@ -9,7 +9,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-
+    <nav class="navbar navbar-expand-lg sticky-top" id="navbar-section">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#" aria-current="home">
+            <img src="/M07/pokemon_php/public/img/pokeball.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-center">
+            <span id="home-title">PokéBlog</span> 
+          </a>
+          <button class="navbar-toggler" id="toggle-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"> <a class="nav-link link-light" href="#" aria-current="pokemon-news">Blog</a></li>
+                <li class="nav-item"> <a class="nav-link link-light" href="#" aria-current="pokemon-images">Galería</a></li>
+                <li class="nav-item"> <a class="nav-link link-light" href="#" aria-current="pokemon-dates">Datos</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
