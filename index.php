cc373ae60068790d09fb825ee91e4508d4637926
AMGG13 <alvinmillergarciagarciagarcia@gmail.com>
2022-10-26 16:44:34 +0200

    añadiendo templates y haciendo el index

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+require_once(__DIR__ . '/utils.php');
+
+//Functions here
+
+
+//__________________________________________________________________________________________
+/**
+ * Main function
+ */
+function main(){
+    //1. Generate index.html from template **index.template.php**
+    //1.1 Put blogs links to the index.html
+    //1.2 Put pokemons links to the index.html
+    
+    
+    //2. Generate blogs.html from template **blog.template.php**
+    //2.1 Get json info of the blog
+    //2.2 Put json info into a generated html file
+
+
+    //3. Generate pokemon_name.html from **pokemon.template.php**
+    //3.1 Get info from PokeApi like name, type, origin ...
+    //3.2 Put info into the table
+}
+
+main();
