78c821a1a1b9aa3568f2f06e9a6f2b6393af5fe8
RaulMOAB <raulmontoroabaijon@gmail.com>
2022-11-01 18:43:34 +0100

    Data table added

--- src/templates/data.template.php
+++ src/templates/data.template.php
@@ -31,9 +31,9 @@
    <div class="container ">
    <span class="logo"><img src="../img/img_aux/pokemon-logo.png" alt="logo-image" width="350" height="100"></span>
         <h1 class="main-title">Aquí están todos los datos de los Pokémons</h1>
-      <div class="container">
+      <div class="table-responsive-lg">
 
-      <table id="data-table" class="table table-dark table-striped">
+      <table id="data-table" class="table table-dark table-hover">
         <?php
         /* $fp= fopen("pokemon.csv","r");
         foreach ($atributes as $rows){
