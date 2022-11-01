1461e3a8dfb4eb074ad0eef5c4ebf69e7fdf725e
AMGG13 <alvinmillergarciacarcia@gmail.com>
2022-11-01 21:17:00 +0100

    regions

--- src/generator.php
+++ src/generator.php
@@ -58,8 +58,8 @@ function get_news_array():array{
  * @return ann array of images
  */
 function get_img_array():array{
-
     $regions_array = ["kanto", "johto"];
+
     $img_array = [];
     $img_paths_array        = glob("../public/img/kanto/*.png");
     $filenames_array        = array_map('get_file_name', $img_paths_array);
@@ -84,6 +84,38 @@ function get_data_array():array{
     }
     return $csv_array;
 }
+
+function get_regions_array():array{
+    $regions = [];
+    $regions_img_array      = glob("../public/img/regions_map/*.webp");
+    $filenames_array        = array_map('get_file_name',$regions_img_array);
+
+    foreach ($filenames_array as $filename) {
+        
+        $region_name = substr($filename,0,-5);
+        $url = "https://pokeapi.co/api/v2/region/$region_name";
+        $json_string = shell_exec("curl $url");
+        $data = json_decode($json_string,true);
+        // $ch = curl_init();
+        // curl_setopt($ch,CURLOPT_URL,"https://pokeapi.co/api/v2/region/$region_name/");
+        // curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        // $json_string = curl_exec($ch);
+        // curl_close($ch);
+        // $data = json_decode($json_string,true);
+
+
+        $version_group = $data["version_groups"];
+
+        $region = [];
+        $region["region_name"] = $filename;
+        $region["game_versions"] =$version_group;
+
+        $regions[$region_name]=$region;
+    }
+    
+    return $regions;
+
+}
 /**
  * Calleable function that returns the file name without the path
  */
@@ -101,6 +133,7 @@ function main(): void
     $blog_filename_template         = "templates/blog.template.php"; 
     $images_filename_template       = "templates/images.template.php";
     $data_filename_template         = "templates/data.template.php";
+    $region_filename_template       = "templates/region.template.php";
     
     
 
@@ -109,6 +142,9 @@ function main(): void
     $blog_filename_html             = "../public/blog.html";
     $images_filename_html           = "../public/images.html";
     $data_filename_html             = "../public/data.html";
+    $regions_filename_html          = "../public/regions.html";
+
+
 
     //TEMPLATE VARS
     //Index
@@ -123,16 +159,22 @@ function main(): void
     $images_array                   = get_img_array();
     $images_template_vars           = ['images'=>$images_array];
 
+    //Regions
+    $regions_array                  = get_regions_array();
+    $regions_template_vars          = ['regions'=>$regions_array];
+
     //Data
     $data_array = get_data_array();
     $data_template_vars =['csv_array' => $data_array];
 
+    print_r($regions_array);
 
 
-    make_index($index_filename_template, $index_filename_html, $index_template_vars);    //Generate index.html
-    make_html($blog_filename_template, $blog_filename_html, $news_template_vars);         //Generate blog.html
-    make_html($images_filename_template, $images_filename_html, $images_template_vars);   //Generate image.html
-    make_html($data_filename_template, $data_filename_html, $data_template_vars);
+    make_index($index_filename_template, $index_filename_html, $index_template_vars);       //Generate index.html
+    make_html($blog_filename_template, $blog_filename_html, $news_template_vars);           //Generate blog.html
+    make_html($images_filename_template, $images_filename_html, $images_template_vars);     //Generate image.html
+    make_html($data_filename_template, $data_filename_html, $data_template_vars);           //Generate data.html
+    //make_html($region_filename_template, $regions_filename_html, $regions_template_vars);   //Generate region.html
 
 
     
