82bb7418e3a2a713947b51ed1acfafdcf4df81bb
Cab0r <mario.barroso.ribes@gmail.com>
2022-11-16 13:57:20 +0100

    Filtar tablas completo

--- src/lib/Table.php
+++ src/lib/Table.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Lib;
 
 class Table
@@ -16,17 +17,18 @@ class Table
     public function __construct(array $header, array $body)
     {
         $this->header = $header;
-        $this->body   = $body;
+        $this->body = $body;
     }
 
     /**
      * Function to get the max length of body
      * @return int $max_length  
      */
-    private function get_max_length():int{
+    private function get_max_length(): int
+    {
         $arr = [];
-        foreach ($this->body as $index => $value) {  
-         $arr [] = max(array_map('strlen',$value));  
+        foreach ($this->body as $index => $value) {
+            $arr[] = max(array_map('strlen', $value));
         }
         $max_length = max($arr);
         return $max_length;
@@ -36,42 +38,43 @@ class Table
      * Function that return the table in a pretty format
      * @return string $string_table header and body table formatted into a string
      */
-    public function __toString():string{
+    public function __toString(): string
+    {
 
-        $max_length = Table::get_max_length();                  //Getting max length of the content
-        $string_table  = '';                                   
+        $max_length = Table::get_max_length(); //Getting max length of the content
+        $string_table = '';
 
         //_________________________HEADER_SET_UP__________________________________
 
-        $header = $this->header;                             //Put header into new variable to not modify the original one
-        foreach ($header as $index => $title) {              // modify index from an indexed array   
-            $header[$index] = str_pad($title,$max_length);   //?Adding to header the value of original header but spaced up to $max_length
+        $header = $this->header; //Put header into new variable to not modify the original one
+        foreach ($header as $index => $title) { // modify index from an indexed array   
+            $header[$index] = str_pad($title, $max_length); //?Adding to header the value of original header but spaced up to $max_length
         }
 
-        $line = str_repeat("═",($max_length*count($header)));//Horizontal line to get more prettier usint str_repeat that repeat a character a number of times, in this case $max_length *  number of columns of the table
+        $line = str_repeat("═", ($max_length * count($header))); //Horizontal line to get more prettier usint str_repeat that repeat a character a number of times, in this case $max_length *  number of columns of the table
 
 
-        $string_table  = $string_table.$line.PHP_EOL;         //Horizontal line
+        $string_table = $string_table . $line . PHP_EOL; //Horizontal line
 
-        $string_header = implode("║ ",$header) . PHP_EOL;     //Header fields separated by an "║" with implode function
-        $string_table  = $string_table . $string_header;      //Concat header fields already formatted in final string
+        $string_header = implode("║ ", $header) . PHP_EOL; //Header fields separated by an "║" with implode function
+        $string_table = $string_table . $string_header; //Concat header fields already formatted in final string
 
-        $string_table  = $string_table.$line.PHP_EOL;         //Horizontal line
+        $string_table = $string_table . $line . PHP_EOL; //Horizontal line
 
         //__________________________BODY_SET_UP____________________________________
 
-        $body_string   = '';                                  
-        $body   = $this->body;                                  //Create body array for not modify the original                         
+        $body_string = '';
+        $body = $this->body; //Create body array for not modify the original                         
         foreach ($body as $content) {
             foreach ($content as $index => $comic_info) {
-                $content[$index] = str_pad((string)$comic_info,$max_length); //Adding to not original body the value of original body but spaced up to $max_length
+                $content[$index] = str_pad((string) $comic_info, $max_length); //Adding to not original body the value of original body but spaced up to $max_length
             }
-            $string_content = implode("║ ", $content) . PHP_EOL;              //Row fields separated by an "║" with implode function 
-            $body_string    = $body_string . $string_content;               
+            $string_content = implode("║ ", $content) . PHP_EOL; //Row fields separated by an "║" with implode function 
+            $body_string = $body_string . $string_content;
         }
 
         $string_table = $string_table . $body_string;
-        $string_table = $string_table.$line.PHP_EOL;
+        $string_table = $string_table . $line . PHP_EOL;
         return $string_table;
     }
 
@@ -85,12 +88,12 @@ class Table
             $csv_array[] = str_getcsv($row);
         }
         //print_r($csv_array);
-        
+
         $header = array_shift($csv_array);
-        $body   = $csv_array;
-                    
-        
-        return [$header,$body];
+        $body = $csv_array;
+
+
+        return [$header, $body];
     }
 
     //*Append a new row into csv array
@@ -105,23 +108,111 @@ class Table
 
     //TODO-->
 
-    //Filtrar filas
-    public function filterRows(int $num_volums){
-        $filter = $num_volums;
+    //Filtrae filas YA VERAS QUE GUAPO NANOOO
+    public function filterRows(int $column, int|string $filter1, int $filter2=null)
+    {
+        if(gettype($column)=="integer"){
+            if(gettype($filter1)=="string"){
+                return $this->_filterStringRows($column,$filter1);
+            }
+            $filter1 = (int)$filter1;
+            if(gettype($filter2)=="integer"){
+                return $this->_filterIntRangeRows($column,$filter1,$filter2);
+            }
+            return $this->_filterIntRows($column,$filter1);
+        }
 
-        $aray_filtered = array_filter($this->body, function ($num_volums) use ($filter){
-           return $num_volums[1] > $filter;//? [1] ??
-        }, ARRAY_FILTER_USE_BOTH);    
+        return $this->body;
+    }
+
+    //Filtrar filas valor unico STRING
+    private function _filterStringRows(int $col, string $filter_string)
+    {
+
+        $aray_filtered = array_filter($this->body, function ($body) use ($filter_string,$col) {
+
+            return str_contains($body[$col], $filter_string);
+            //return $body[1] == $filter;
+        }, ARRAY_FILTER_USE_BOTH);
+
+
+        return $aray_filtered;
+    }
+    //Filtrar filas valor unico NUMERICO
+    private function _filterIntRows(int $col, int $filter_int)
+    {
+
+        $aray_filtered = array_filter($this->body, function ($body) use ($filter_int,$col) {
+
+            return $body[$col] == $filter_int;
+            
+        }, ARRAY_FILTER_USE_BOTH);
+
+
+        return $aray_filtered;
+    }
+    //Filtrar filas valor RANGO DE NUMEROS
+    private function _filterIntRangeRows( int $col,int $filter_min_int, int $filter_max_int)
+    {
+        
+        if ($filter_max_int<$filter_min_int){
+            $aux = $filter_max_int;
+            $filter_max_int=$filter_min_int;
+            $filter_min_int=$aux;
+        }
+        $aray_filtered = array_filter($this->body, function ($body) use ($filter_min_int, $filter_max_int, $col) {
+
+            return $body[$col] >= $filter_min_int && $body[$col] <= $filter_max_int;
+            
+        }, ARRAY_FILTER_USE_BOTH);
+
+
         return $aray_filtered;
     }
 
     //Obtener columna
-    public function getRow(int $row):array{
+    public function getRow(int $row): array
+    {
         return $this->body[$row];
     }
-    
-    public function getColumn(int $col):array{
+
+    public function getColumn(int $col): array
+    {
         return array_column($this->body, $col);
     }
 }
 
+function main()
+{
+    $manga = new Table(
+        ['Titulo', 'Tomos'],
+        [
+            ['Chainsaw Man',    13],
+            ['Attack on Titan', 27],
+            ['Titan Paco',      31],
+            ['Dragon Ball Z',   10],
+            ['One Piece',       70]
+        ]
+    );
+
+
+    $filtered_str = new Table(
+        ['Titulo Titan', 'Tomos'],
+        $manga->filterRows(0,'Titan')
+    );
+    $filtered_int = new Table(
+        ['Titulo Titan', 'Tomos ¡27!'],
+        $manga->filterRows(1,27)
+    );
+    $filtered_rng = new Table(
+        ['Titulo', 'Tomos 10 a 30'],
+        $manga->filterRows(1,10,30)
+    );
+
+    echo $manga->__toString();
+    echo $filtered_str->__toString();
+    echo $filtered_int->__toString();
+    echo $filtered_rng->__toString();
+}
+
+main();
